5b85e5c41c440b3e2a14cf4c073c6d918bb28f16
Phill Tew <peatew@gmail.com>
2022-12-03 16:36:54 +0000

    👏 Pose: add locking to pose variable subscriber

--- Ossify/Ossify/Variables/PoseSubscriber.cs
+++ Ossify/Ossify/Variables/PoseSubscriber.cs
@@ -6,12 +6,39 @@ namespace Ossify.Variables
     {
         [SerializeField] private PoseVariable pose;
 
+        public bool LockPositionX;
+        public bool LockPositionY;
+        public bool LockPositionZ;
+        
+        public bool LockRotationPitch;
+        public bool LockRotationRoll;
+        public bool LockRotationYaw;
+
         private void Update()
         {
             Transform t = transform;
 
-            t.position = pose.Value.position;
-            t.rotation = pose.Value.rotation;
+            var originalPosition = t.position;
+            var posePosition = pose.Value.position;
+            
+            var newPosition = new Vector3(
+                LockPositionX ? originalPosition.x : posePosition.x,
+                LockPositionY ? originalPosition.y : posePosition.y,
+                LockPositionZ ? originalPosition.z : posePosition.z
+            );
+            
+            t.position = newPosition;
+            
+            var originalRotation = t.eulerAngles;
+            var poseRotation = pose.Value.rotation.eulerAngles;
+            
+            var newRotation = new Vector3(
+                LockRotationPitch ? originalRotation.x : poseRotation.x,
+                LockRotationYaw ? originalRotation.y : poseRotation.y,
+                LockRotationRoll ? originalRotation.z : poseRotation.z
+            );
+
+            t.eulerAngles = newRotation;
         }
     }
 }
