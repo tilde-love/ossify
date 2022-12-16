c047419164103c200b650676900e8f0329cf0a68
Phill Tew <peatew@gmail.com>
2022-12-16 23:54:03 +0000

    👏 Text Bindings: add text bindings for int and bool types

---
+++ Ossify/Ossify/Bindings/Specific/TMP/TextToIntVariableBinder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Ossify.Variables;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Ossify.Bindings.Specific.TMP
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public sealed class TextToIntVariableBinder : MonoBehaviour
+    {
+        [SerializeField] private IntVariable variable;
+
+        [SerializeField] private StringReference format = new() { Value = "{0}" };
+
+        [SerializeField] private IntEvent onValueChanged = new();
+
+        private TMP_Text bound;
+
+        private int? value;
+
+        private void Awake() => bound = GetComponent<TMP_Text>();
+
+        private void OnEnable()
+        {
+            value = null;
+
+            variable.ValueChanged += OnValueChanged;
+
+            OnValueChanged(variable.Value);
+        }
+
+        private void OnDisable() => variable.ValueChanged -= OnValueChanged;
+
+        private void OnValueChanged(int value)
+        {
+            if (this.value != null && this.value.Value == value)
+            {
+                return;
+            }
+
+            this.value = value;
+
+            bound.text = string.Format(format.Value, value);
+
+            onValueChanged.Invoke(value);
+        }
+
+        [Serializable]
+        public sealed class IntEvent : UnityEvent<int>
+        {
+        }
+    }
+}
